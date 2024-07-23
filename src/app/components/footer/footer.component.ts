c5c7e544f6fdf81af7bddd6ea284e1a2462da33c
Maria Alarcon <mariagaa993@gmail.com>
2024-07-23 11:25:43 -0300

    footer design

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhone, faMapMarkerAlt, faEnvelope, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'footer-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faPhone = faPhone;
+  faMapMarkerAlt = faMapMarkerAlt;
+  faInstagram = faInstagram;
+  faEnvelope = faEnvelope;
+  faClock = faClock;
 }
